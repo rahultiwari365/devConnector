874d0174003f5f82390cd51485c25dfc584a30d1
Rahul Tiwari <rahul.tiwari@Triconinfotech.com>
2020-12-22 18:10:08 +0530

    \Prepared for deployment

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
+import PostForm from "./PostForm";
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  console.log("posts-", posts);
+
+  return <Fragment>
+  { loading ? <Spinner /> : <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+          <i className="fab fa-user"></i> Welcome to community
+      </p>
+      <PostForm />
+      <div className="posts">
+          { !posts ? <h4>No Posts found...</h4> : (
+              posts.map(post => {
+                 return <PostItem key={post._id} post={post} />
+              })
+          )}
+      </div>
+      </Fragment>}
+    </Fragment>
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
